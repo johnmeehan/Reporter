d0471ebaefee5f3aa8bac174875845722ee545cc
John Meehan <john.meehan1@gmail.com>
2015-07-03 12:00:23 +0100

    added ES to Record and callbacks

--- app/models/record.rb
+++ app/models/record.rb
@@ -74,4 +74,6 @@
 #
 
 class Record < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -76,71 +76,71 @@
 FactoryGirl.define do
   factory :record do
     version 1
-current_holding ""
-previous_holding ""
-market_value "9.99"
-previous_market_value "9.99"
-pct_held "9.99"
-pct_portfolio "MyString"
-market_capitalization "MyString"
-q0_holding 1
-q0_quarter "2015-07-03 11:34:13"
-q0_source "MyString"
-q1_holding 1
-q1_quarter "2015-07-03 11:34:13"
-q1_source "MyString"
-q2_holding 1
-q2_quarter "2015-07-03 11:34:13"
-q2_source "MyString"
-q3_holding 1
-q3_quarter "2015-07-03 11:34:13"
-q3_source "MyString"
-q4_holding 1
-q4_quarter "2015-07-03 11:34:13"
-q4_source "MyString"
-q5_holding 1
-q5_quarter "2015-07-03 11:34:13"
-q5_source "MyString"
-q6_holding 1
-q6_quarter "2015-07-03 11:34:13"
-q6_source "MyString"
-q7_holding 1
-q7_quarter "2015-07-03 11:34:13"
-q7_source "MyString"
-q8_holding 1
-q8_quarter "2015-07-03 11:34:13"
-q8_source "MyString"
-p0_price "9.99"
-p1_price "9.99"
-p4_price "9.99"
-metro_area "MyString"
-firm_name "MyString"
-company_name "MyString"
-ticker "MyString"
-q0_market_value "9.99"
-qoq_net_change_flow 1
-yoy_net_change_flow 1
-qoq_inflow 1
-qoq_outflow 1
-yoy_inflow 1
-yoy_outflow 1
-qoq_new_positions 1
-qoq_soldout_positions 1
-qoq_increased_positions 1
-qoq_decreased_positions 1
-qoq_net_position_change 1
-yoy_new_positions 1
-yoy_soldout_positions 1
-yoy_increased_positions 1
-yoy_decreased_positions 1
-yoy_net_position_change 1
-new_positions 1
-soldout_positions 1
-increased_positions 1
-decreased_positions 1
-net_position_change 1
-factset_security_id 1
-factset_entity_id 1
+    current_holding ""
+    previous_holding ""
+    market_value "9.99"
+    previous_market_value "9.99"
+    pct_held "9.99"
+    pct_portfolio "MyString"
+    market_capitalization "MyString"
+    q0_holding 1
+    q0_quarter "2015-07-03 11:34:13"
+    q0_source "MyString"
+    q1_holding 1
+    q1_quarter "2015-07-03 11:34:13"
+    q1_source "MyString"
+    q2_holding 1
+    q2_quarter "2015-07-03 11:34:13"
+    q2_source "MyString"
+    q3_holding 1
+    q3_quarter "2015-07-03 11:34:13"
+    q3_source "MyString"
+    q4_holding 1
+    q4_quarter "2015-07-03 11:34:13"
+    q4_source "MyString"
+    q5_holding 1
+    q5_quarter "2015-07-03 11:34:13"
+    q5_source "MyString"
+    q6_holding 1
+    q6_quarter "2015-07-03 11:34:13"
+    q6_source "MyString"
+    q7_holding 1
+    q7_quarter "2015-07-03 11:34:13"
+    q7_source "MyString"
+    q8_holding 1
+    q8_quarter "2015-07-03 11:34:13"
+    q8_source "MyString"
+    p0_price "9.99"
+    p1_price "9.99"
+    p4_price "9.99"
+    metro_area "MyString"
+    firm_name "MyString"
+    company_name "MyString"
+    ticker "MyString"
+    q0_market_value "9.99"
+    qoq_net_change_flow 1
+    yoy_net_change_flow 1
+    qoq_inflow 1
+    qoq_outflow 1
+    yoy_inflow 1
+    yoy_outflow 1
+    qoq_new_positions 1
+    qoq_soldout_positions 1
+    qoq_increased_positions 1
+    qoq_decreased_positions 1
+    qoq_net_position_change 1
+    yoy_new_positions 1
+    yoy_soldout_positions 1
+    yoy_increased_positions 1
+    yoy_decreased_positions 1
+    yoy_net_position_change 1
+    new_positions 1
+    soldout_positions 1
+    increased_positions 1
+    decreased_positions 1
+    net_position_change 1
+    factset_security_id 1
+    factset_entity_id 1
   end
 
 end
