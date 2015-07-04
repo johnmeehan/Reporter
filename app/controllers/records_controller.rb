9b603b2abee3811efcb59fb70ee2c91f8f7d80cd
John Meehan <john.meehan1@gmail.com>
2015-07-04 17:18:15 +0100

    Sortable columns

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,13 @@
 class RecordsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   before_action :set_record, only: [:show, :edit, :update, :destroy]
 
   def index
     version_info
     # Only Show recent versions and paginate
-    @records = Record.where(version: @latestversion).page(params[:page]).per(20)
+    @records = Record.where(version: @latestversion).
+        order(sort_column + ' ' + sort_direction )
+        .page(params[:page]).per(20)
     # @records = Record.search(version: @latestversion)
     # @records = Record.search(version: @latestversion).records.page(params[:page]).per(20)
     # @records = Record.search( version: @latestversion).page(params[:page]).records
@@ -12,7 +15,6 @@ class RecordsController < ApplicationController
     # @records = Record.search(version: @latestversion).page(params[:page]).records
     # @records = response
   end
-
 
   def show
   end
@@ -69,6 +71,14 @@ class RecordsController < ApplicationController
     end
 
     def record_params
-      params.require(:record).permit(:version, :current_holdings, :previous_holdings, :market_value, :previous_market_value, :pct_held, :pct_portfolio, :market_capitalization, :q0_holding, :q0_quarter, :q0_source, :q1_holding, :q1_quarter, :q1_source, :q2_holding, :q2_quarter, :q2_source, :q3_holding, :q3_quarter, :q3_source, :q4_holding, :q4_quarter, :q4_source, :q5_holding, :q5_quarter, :q5_source, :q6_holding, :q6_quarter, :q6_source, :q7_holding, :q7_quarter, :q7_source, :q8_holding, :q8_quarter, :q8_source, :p0_price, :p1_price, :p4_price, :metro_area, :firm_name, :company_name, :ticker, :q0_market_value, :qoq_net_change_flow, :yoy_net_change_flow, :qoq_inflow, :qoq_outflow, :yoy_inflow, :yoy_outflow, :qoq_new_positions, :qoq_soldout_positions, :qoq_increased_positions, :qoq_decreased_positions, :qoq_net_position_change, :yoy_new_positions, :yoy_soldout_positions, :yoy_increased_positions, :yoy_decreased_positions, :yoy_net_position_change, :new_positions, :soldout_positions, :increased_positions, :decreased_positions, :net_position_change, :factset_security_id, :factset_entity_id)
+      params.require(:record).permit(:sort, :class, :direction, :version, :current_holdings, :previous_holdings, :market_value, :previous_market_value, :pct_held, :pct_portfolio, :market_capitalization, :q0_holding, :q0_quarter, :q0_source, :q1_holding, :q1_quarter, :q1_source, :q2_holding, :q2_quarter, :q2_source, :q3_holding, :q3_quarter, :q3_source, :q4_holding, :q4_quarter, :q4_source, :q5_holding, :q5_quarter, :q5_source, :q6_holding, :q6_quarter, :q6_source, :q7_holding, :q7_quarter, :q7_source, :q8_holding, :q8_quarter, :q8_source, :p0_price, :p1_price, :p4_price, :metro_area, :firm_name, :company_name, :ticker, :q0_market_value, :qoq_net_change_flow, :yoy_net_change_flow, :qoq_inflow, :qoq_outflow, :yoy_inflow, :yoy_outflow, :qoq_new_positions, :qoq_soldout_positions, :qoq_increased_positions, :qoq_decreased_positions, :qoq_net_position_change, :yoy_new_positions, :yoy_soldout_positions, :yoy_increased_positions, :yoy_decreased_positions, :yoy_net_position_change, :new_positions, :soldout_positions, :increased_positions, :decreased_positions, :net_position_change, :factset_security_id, :factset_entity_id)
+    end
+
+    def sort_column
+      Record.column_names.include?(params[:sort]) ? params[:sort] : "version"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def sortable(column, title = nil)
+    title ||= column
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_column && sort_direction == "desc") ? "asc" : "desc"
+    link_to title, {:sort => column.parameterize('_'), :direction => direction}, class: css_class
+  end
+
 end
