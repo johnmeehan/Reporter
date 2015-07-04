303897d4b17ad31879c9daa087fc19250e3439b3
John Meehan <john.meehan1@gmail.com>
2015-07-04 17:30:34 +0100

    columns now sort direction can be toggled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
   def sortable(column, title = nil)
     title ||= column
-    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
-    direction = (column == sort_column && sort_direction == "desc") ? "asc" : "desc"
+    css_class = (column.parameterize('_') == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column.parameterize('_') == sort_column && sort_direction == "desc") ? "asc" : "desc"
     link_to title, {:sort => column.parameterize('_'), :direction => direction}, class: css_class
   end
 
