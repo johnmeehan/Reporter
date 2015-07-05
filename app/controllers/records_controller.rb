72f0600afeb7f3dddb68e68f202cd493bfd29dcc
John Meehan <john.meehan1@gmail.com>
2015-07-05 17:15:06 +0100

    Admin area for selecting table columns to display

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,19 +3,16 @@ class RecordsController < ApplicationController
   before_action :set_record, only: [:show, :edit, :update, :destroy]
 
   def index
+    get_column_names
     set_visabilty
-    get_columns
+
     version_info
-    # Only Show recent versions and paginate
-    @records = Record.where(version: @latestversion)
-        .order(sort_column + ' ' + sort_direction )
-        .page(params[:page]).per(20)
-    # @records = Record.search(version: @latestversion)
-    # @records = Record.search(version: @latestversion).records.page(params[:page]).per(20)
-    # @records = Record.search( version: @latestversion).page(params[:page]).records
-    # @records = Record.search(params[:q]).page(params[:page]).records
-    # @records = Record.search(version: @latestversion).page(params[:page]).records
-    # @records = response
+    # # Only Show recent versions and paginate
+    response = Record.where(version: @latestversion).order(sort_column + ' ' + sort_direction ).page(params[:page]).per(20)
+    # q = @latestversion
+    # response = Record.search(q).page(params[:page]).records
+    @records = response
+
   end
 
   def show
@@ -72,12 +69,9 @@ class RecordsController < ApplicationController
       @record = Record.find(params[:id])
     end
 
-    def record_params
-      params.require(:record).permit(:sort, :class, :direction, :version, :current_holdings, :previous_holdings, :market_value, :previous_market_value, :pct_held, :pct_portfolio, :market_capitalization, :q0_holding, :q0_quarter, :q0_source, :q1_holding, :q1_quarter, :q1_source, :q2_holding, :q2_quarter, :q2_source, :q3_holding, :q3_quarter, :q3_source, :q4_holding, :q4_quarter, :q4_source, :q5_holding, :q5_quarter, :q5_source, :q6_holding, :q6_quarter, :q6_source, :q7_holding, :q7_quarter, :q7_source, :q8_holding, :q8_quarter, :q8_source, :p0_price, :p1_price, :p4_price, :metro_area, :firm_name, :company_name, :ticker, :q0_market_value, :qoq_net_change_flow, :yoy_net_change_flow, :qoq_inflow, :qoq_outflow, :yoy_inflow, :yoy_outflow, :qoq_new_positions, :qoq_soldout_positions, :qoq_increased_positions, :qoq_decreased_positions, :qoq_net_position_change, :yoy_new_positions, :yoy_soldout_positions, :yoy_increased_positions, :yoy_decreased_positions, :yoy_net_position_change, :new_positions, :soldout_positions, :increased_positions, :decreased_positions, :net_position_change, :factset_security_id, :factset_entity_id)
-    end
 
     def sort_column
-      Record.column_names.include?(params[:sort]) ? params[:sort] : "version"
+      @columns.include?(params[:sort]) ? params[:sort] : "version"
     end
 
     def sort_direction
@@ -86,16 +80,15 @@ class RecordsController < ApplicationController
 
     def set_visabilty
       unless session[:visible]
-        session[:visible]||= {}
-        get_columns.each do |col|
+        session[:visible] = {}
+        @columns.each do |col|
           session[:visible][col.to_sym] = true
         end
         # session[:visible].except!(:created_at, :updated_at, :id)
       end
     end
 
-    def get_columns
-      # @columns = Record.columns_hash.except!(:created_at, :updated_at, :id)
-      @columns = Record.column_names.reject { |x| ['created_at', 'updated_at', 'id'].include? x}
+    def get_column_names
+      @columns = Record.record_column_names
     end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -77,24 +77,25 @@ class Record < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
-  # def self.search(query)
-  # __elasticsearch__.search(
-  #   {
-  #     query: {
-  #       multi_match: {
-  #         query: query,
-  #         fields: ['version^10', 'content']
-  #       }
-  #     },
-  #     highlight: {
-  #       pre_tags: ['<em class="label label-highlight">'],
-  #       post_tags: ['</em>'],
-  #       fields: {
-  #         title:   { number_of_fragments: 0 },
-  #         content: { fragment_size: 25 }
-  #       }
-  #     }
-  #   }
-  # )
-  # end
+  def self.record_column_names
+    # @columns = Record.columns_hash.except!(:created_at, :updated_at, :id)
+    self.column_names.reject { |x| ['created_at', 'updated_at', 'id'].include? x}
+  end
+
+  def self.search(query)
+  __elasticsearch__.search(
+    {
+      query: {
+        multi_match: {
+          query: query,
+          fields: ['version']
+        }
+      },
+      highlight: {
+        pre_tags: ['<em class="label label-highlight">'],
+        post_tags: ['</em>']
+      }
+    }
+  )
+  end
 end
