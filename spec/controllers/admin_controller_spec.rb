75d30cb300e221b6d239b71e76e7e0bda0b2867d
John Meehan <john.meehan1@gmail.com>
2015-07-05 13:40:28 +0100

    RSpec test for basic authentication

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  context 'with valid credentials' do
+    describe 'GET #index' do
+      it "returns success" do
+        http_login
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
+  context 'without credentials' do
+    describe "GET #index" do
+      it "returns http unauthorized" do
+        get :index
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
 end
---
+++ spec/support/auth_helper.rb
@@ -0,0 +1,29 @@
+module AuthHelper
+  def http_login
+    user = 'admin'
+    pw = 'password'
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
+  end
+end
+
+module AuthRequestHelper
+  #
+  # pass the @env along with your request, eg:
+  #
+  # GET '/labels', {}, @env
+  #
+  def http_login
+    @env ||= {}
+    user = 'admin'
+    pw = 'password'
+    @env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
+  end
+end
+
+
+# then in Rspec support:
+
+RSpec.configure do |config|
+  config.include AuthRequestHelper, :type => :request
+  config.include AuthHelper, :type => :controller
+end
