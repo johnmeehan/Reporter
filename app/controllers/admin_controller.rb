7bd07cd6a0edb8e5ddad9a4105671a40bfc602bd
John Meehan <john.meehan1@gmail.com>
2015-07-06 20:21:19 +0100

    removed unused code and clutter

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,19 +1,8 @@
 class AdminController < ApplicationController
-  # Spec said basic authenciation.....
   http_basic_authenticate_with name: 'admin', password: 'password'
 
   def index
     @columns = Record.record_column_names
-  end
-
-  def set_columns
-    if params[:visible]
-      session[:visible] ||= {}
-      visibiltiy_params.each do |key, value|
-        session[:visible][key.to_sym] = value
-      end
-    end
-    redirect_to root_url
   end
 
   def show
