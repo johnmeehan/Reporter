28a53b37eb288662d0a9d75a96e8ca9ba60f2b6b
John Meehan <john.meehan1@gmail.com>
2015-07-10 19:57:01 +0100

    added some error handling for bad search requests. i.e. when sorting a column with no contents. It now handles safely when elasticsearch throws an error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Elasticsearch::Transport::Transport::Errors::BadRequest do |error|
+    if error.message =~ /SearchParseException/ && params[:query]
+      flash[:error] = "You tried to sort an empty column!"
+    end
+      render file: "/public/400.html",status: 400, layout: true
+  end
+
 end
